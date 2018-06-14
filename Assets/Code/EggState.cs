1d87f477b9aa34520eb402c9857364b6af08609e
Unknown <hengdxl@hotmail.com>
2018-06-14 21:19:43 +0700

    darkhager-1
    -change folder pattern
    -add loading scene
    -add picture

---
+++ Assets/Code/EggState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggState : MonoBehaviour {
+
+    Animator anim;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetBool("ClickState", true);
+        } else
+        {
+            anim.SetBool("ClickState", false);
+        }
+    }
+}
---
+++ Assets/Code/TextTyper.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextTyper : MonoBehaviour
+{
+
+    public float letterPause = 0.4f; 
+    string message;
+    Text textComp;
+
+    void Start()
+    {
+        StartCoroutine(TypeText());
+    }
+
+    IEnumerator TypeText()
+    {
+        for(; ; ){
+            textComp = GetComponent<Text>();
+            message = textComp.text;
+            textComp.text = "";
+            foreach (char letter in message.ToCharArray()){
+                textComp.text += letter;
+                yield return 0;
+                yield return new WaitForSeconds(letterPause);
+            }
+        }
+        
+    }
+}
